6cfa4990f675c12b2c82e33b39f4d168e784f505
김현승 <corn120033@gmail.com>
2024-03-30 21:50:47 +0900

    의존성 역전 원칙 WorstPractice 작성

---
+++ Assets/Scripts/DependencyInversion/WorstPractice/Door.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DependencyInversion.WorstPractice
+{
+    public class Door:MonoBehaviour
+    {
+        public void Open()
+        {
+            Debug.Log("The door is open.");
+        }
+
+        public void Close()
+        {
+            Debug.Log("The door is closed.");
+        }
+    }
+}
---
+++ Assets/Scripts/DependencyInversion/WorstPractice/Switch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DependencyInversion.WorstPractice
+{
+    public class Switch : MonoBehaviour
+    {
+        public Door Door;
+        public bool IsActivated;
+
+        public void Toggle()
+        {
+            if (IsActivated)
+            {
+                IsActivated = false;
+                Door.Close();
+            }
+            else
+            {
+                IsActivated = true;
+                Door.Open();
+            }
+        }
+    }
+}
