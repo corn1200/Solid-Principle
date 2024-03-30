376a70195a85f3a8e2477eb25a6fd55b5fd2a11f
김현승 <corn120033@gmail.com>
2024-03-30 21:15:41 +0900

    인터페이스 분리 원칙 WorstPractice 작성

---
+++ Assets/Scripts/InterfaceSegregation/WorstPractice/IUnitStats.cs
@@ -0,0 +1,13 @@
+﻿namespace InterfaceSegregation.WorstPractice
+{
+    public interface IUnitStats
+    {
+        public float Health { get; set; }
+        public int Defense { get; set; }
+        public void TakeDamage();
+        public float MoveSpeed { get; set; }
+        public void GoForward();
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+    }
+}
